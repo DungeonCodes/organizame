198614eb742f3403708206a9be489f82e28eb6e7
Rafael Gonçalves Martins <128759336+DungeonCodes@users.noreply.github.com>
2025-05-04 16:49:59 -0300

    guicorn

--- organizame/run.py
+++ organizame/run.py
@@ -8,5 +8,5 @@ def hello():
     return 'Flask está rodando com sucesso no Railway!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))  # Railway define a porta pela variável PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
