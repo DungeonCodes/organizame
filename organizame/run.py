d2def60a66de204b8892034fa1fcef2f6353c1d8
Rafael Gonçalves Martins <128759336+DungeonCodes@users.noreply.github.com>
2025-05-04 16:44:42 -0300

    Update run.py

--- organizame/run.py
+++ organizame/run.py
@@ -1,8 +1,12 @@
-from app import create_app
+from flask import Flask
 import os
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Flask está rodando com sucesso no Railway!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Pega a porta definida pelo Railway
-    app.run(host='0.0.0.0', port=port, debug=True)  # Expõe para o exterior
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
